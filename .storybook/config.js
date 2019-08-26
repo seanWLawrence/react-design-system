86bdee09876955fb2bfc6b9e752dff0238acb131
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-08-26 08:57:20 -0700

    add a11y storybook extension

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,19 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs } from '@storybook/addon-knobs';
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../packages', true, /\.stories\.tsx$/);
+
+/**
+ * Show accessibility results
+ */
+addDecorator(withA11y);
+
+/**
+ * Allow props to be dynamically updated
+ */
+addDecorator(withKnobs);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
