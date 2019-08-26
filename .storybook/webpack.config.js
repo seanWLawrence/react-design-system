6f47decd36e951f0c1f823a6aa4d4b95b7d3f38c
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-08-26 11:01:46 -0700

    add sass to storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,5 @@
+let path = require('path');
+
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
@@ -7,6 +9,14 @@ module.exports = ({ config }) => {
       },
     ],
   });
+
+  config.module.rules.push({
+    test: /\.sass$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../packages'),
+  });
+
   config.resolve.extensions.push('.ts', '.tsx');
+
   return config;
 };
