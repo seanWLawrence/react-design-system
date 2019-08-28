dbfc8d518fb958c2daf9e2b9f1920fac76a614f8
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-08-28 08:33:43 -0700

    remove info options for stories since they're in the default config now

--- packages/typography/src/heading/index.stories.tsx
+++ packages/typography/src/heading/index.stories.tsx
@@ -6,80 +6,68 @@ import Heading from './';
 
 const headingStories = storiesOf('Typography/Heading', module);
 
-headingStories.add(
-  'All',
-  () => (
-    <>
-      <Heading
-        variants={{
-          type: 'h1',
-        }}
-      >
-        Heading 1
-      </Heading>
-
-      <Heading
-        variants={{
-          type: 'h2',
-        }}
-      >
-        Heading 2
-      </Heading>
-
-      <Heading
-        variants={{
-          type: 'h3',
-        }}
-      >
-        Heading 3
-      </Heading>
-
-      <Heading
-        variants={{
-          type: 'h4',
-        }}
-      >
-        Heading 4
-      </Heading>
-
-      <Heading
-        variants={{
-          type: 'h5',
-        }}
-      >
-        Heading 5
-      </Heading>
-
-      <Heading
-        variants={{
-          type: 'h6',
-        }}
-      >
-        Heading 6
-      </Heading>
-    </>
-  ),
-  {
-    info: { inline: true },
-  }
-);
-
-headingStories.add(
-  'With knobs',
-  () => (
+headingStories.add('All', () => (
+  <>
     <Heading
       variants={{
-        type: select(
-          'Type',
-          { H1: 'h1', H2: 'h2', H3: 'h3', H4: 'h4', H5: 'h5', H6: 'h6' },
-          'h1'
-        ),
+        type: 'h1',
       }}
     >
-      {text('Text', 'Heading')}
+      Heading 1
     </Heading>
-  ),
-  {
-    info: { inline: true },
-  }
-);
+
+    <Heading
+      variants={{
+        type: 'h2',
+      }}
+    >
+      Heading 2
+    </Heading>
+
+    <Heading
+      variants={{
+        type: 'h3',
+      }}
+    >
+      Heading 3
+    </Heading>
+
+    <Heading
+      variants={{
+        type: 'h4',
+      }}
+    >
+      Heading 4
+    </Heading>
+
+    <Heading
+      variants={{
+        type: 'h5',
+      }}
+    >
+      Heading 5
+    </Heading>
+
+    <Heading
+      variants={{
+        type: 'h6',
+      }}
+    >
+      Heading 6
+    </Heading>
+  </>
+));
+
+headingStories.add('With knobs', () => (
+  <Heading
+    variants={{
+      type: select(
+        'Type',
+        { H1: 'h1', H2: 'h2', H3: 'h3', H4: 'h4', H5: 'h5', H6: 'h6' },
+        'h1'
+      ),
+    }}
+  >
+    {text('Text', 'Heading')}
+  </Heading>
+));
--- packages/ui/src/button/index.stories.tsx
+++ packages/ui/src/button/index.stories.tsx
@@ -32,136 +32,124 @@ let Row: FC<{ children: ReactNode; title?: string }> = ({
 
 let Margin = () => <div style={{ marginBottom: 40 }}></div>;
 
-buttonStories.add(
-  'All',
-  () => (
-    <>
-      <Row title="Primary">
-        <Button
-          variants={{
-            type: 'primary',
-            style: 'solid',
-          }}
-        >
-          Save
-        </Button>
+buttonStories.add('All', () => (
+  <>
+    <Row title="Primary">
+      <Button
+        variants={{
+          type: 'primary',
+          style: 'solid',
+        }}
+      >
+        Save
+      </Button>
 
-        <Button
-          variants={{
-            type: 'primary',
-            style: 'outlined',
-          }}
-        >
-          Learn more
-        </Button>
+      <Button
+        variants={{
+          type: 'primary',
+          style: 'outlined',
+        }}
+      >
+        Learn more
+      </Button>
 
-        <Button
-          variants={{
-            type: 'primary',
-            style: 'text',
-          }}
-        >
-          Learn more
-        </Button>
-      </Row>
+      <Button
+        variants={{
+          type: 'primary',
+          style: 'text',
+        }}
+      >
+        Learn more
+      </Button>
+    </Row>
 
-      <Margin />
+    <Margin />
 
-      <Row title="Secondary">
-        <Button
-          variants={{
-            type: 'secondary',
-            style: 'solid',
-          }}
-        >
-          Cancel
-        </Button>
+    <Row title="Secondary">
+      <Button
+        variants={{
+          type: 'secondary',
+          style: 'solid',
+        }}
+      >
+        Cancel
+      </Button>
 
-        <Button
-          variants={{
-            type: 'secondary',
-            style: 'outlined',
-          }}
-        >
-          Cancel
-        </Button>
+      <Button
+        variants={{
+          type: 'secondary',
+          style: 'outlined',
+        }}
+      >
+        Cancel
+      </Button>
 
-        <Button
-          variants={{
-            type: 'secondary',
-            style: 'text',
-          }}
-        >
-          Learn more
-        </Button>
-      </Row>
+      <Button
+        variants={{
+          type: 'secondary',
+          style: 'text',
+        }}
+      >
+        Learn more
+      </Button>
+    </Row>
 
-      <Margin />
+    <Margin />
 
-      <Row title="Destructive">
-        <Button
-          variants={{
-            type: 'destructive',
-            style: 'solid',
-          }}
-        >
-          Delete
-        </Button>
+    <Row title="Destructive">
+      <Button
+        variants={{
+          type: 'destructive',
+          style: 'solid',
+        }}
+      >
+        Delete
+      </Button>
 
-        <Button
-          variants={{
-            type: 'destructive',
-            style: 'outlined',
-          }}
-        >
-          Reset
-        </Button>
+      <Button
+        variants={{
+          type: 'destructive',
+          style: 'outlined',
+        }}
+      >
+        Reset
+      </Button>
 
-        <Button
-          variants={{
-            type: 'destructive',
-            style: 'text',
-          }}
-        >
-          Dismiss
-        </Button>
-      </Row>
-    </>
-  ),
-  {
-    info: { inline: true },
-  }
-);
+      <Button
+        variants={{
+          type: 'destructive',
+          style: 'text',
+        }}
+      >
+        Dismiss
+      </Button>
+    </Row>
+  </>
+));
 
-buttonStories.add(
-  'With knobs',
-  () => (
-    <Button
-      variants={{
-        type: select(
-          'Color',
-          {
-            Primary: 'primary',
-            Secondary: 'secondary',
-            Destructive: 'destructive',
-          },
-          'primary'
-        ),
-        style: select(
-          'Style',
-          {
-            Solid: 'solid',
-            Outlined: 'outlined',
-            Text: 'text',
-          },
-          'solid'
-        ),
-      }}
-    >
-      {text('Text', 'Save')}
-    </Button>
-  ),
-  {
-    info: { inline: true },
-  }
-);
+buttonStories.add('With knobs', () => (
+  <Button
+    variants={{
+      type: select(
+        'Color',
+        {
+          Primary: 'primary',
+          Secondary: 'secondary',
+          Destructive: 'destructive',
+        },
+        'primary'
+      ),
+      style: select(
+        'Style',
+        {
+          Solid: 'solid',
+          Outlined: 'outlined',
+          Text: 'text',
+        },
+        'solid'
+      ),
+    }}
+  >
+    {text('Text', 'Save')}
+  </Button>
+));
